c97446acf30096c95be1f641e039ef0e193c09b3
Joao Cerqueira <joaodanielcerqueira@hotmail.com>
2017-10-26 14:23:24 +0100

    Ficha3-Ex.1

---
+++ Ficha3/Ex.1/main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: joaod
+ *
+ * Created on 26 de Outubro de 2017, 14:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    
+    int num = 1;
+    
+    for(num == 1; num <= 100; ++num){
+        printf("%d\n", num);
+    }
+
+    return (0);
+}
+
