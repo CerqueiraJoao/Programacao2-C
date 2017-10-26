8c912543649eda1d1d5c2621cefc360566fdf6e6
Joao Cerqueira <joaodanielcerqueira@hotmail.com>
2017-10-26 14:10:18 +0100

    Ficha2-Ex.15

--- Ficha2/Ex.15/main.c
+++ Ficha2/Ex.15/main.c
@@ -13,26 +13,31 @@
 int main(int argc, char** argv) {
 
     int viagem;
-    float media, consumo, combustivel;
+    float media, autonomia, combustivel;
     puts ("Em quanto tempo pretende fazer a viagem (Minutos)");
     scanf("%d", &viagem);
     
     media = distancia / (viagem / 60.0);
     
     if (media <= 60){
-        combustivel = 5;             
-        printf ("Tem que abastecer ao km 79.\n");
-        printf("A viagem será efetuada a uma velocidade média de %.2fkm/h", media);    
+        combustivel = 5; 
         }else if (media > 60 && media <= 120){
-        combustivel = 6;
-        printf ("Tem que abastecer ao km 70.\n");
-        printf("A viagem será efetuada a uma velocidade média de %.2fkm/h", media);    
-        }else{
-        combustivel = 7;
-        printf ("Tem que abastecer ao km 60.\n");
+        combustivel = 6;      
+        }else if (media > 120){
+        combustivel = 7;  
+        }
+
+        autonomia = (litros * 100) / combustivel;
+        if (autonomia >= 79 && autonomia < 130){
+            printf ("Tem que abastecer, obrigatóriamente, ao km 79.\n");
+        }else if (autonomia >= 60 && autonomia < 70){
+            printf ("Tem que abastecer, obrigatóriamente, ao km 60.\n");
+        }else if (autonomia >= 70 && autonomia < 79){
+            printf ("Tem que abastecer, obrigatóriamente, ao km 70.\n");
+        }
+        printf("Com o combustivel atual faz apenas %.2fkm.\n", autonomia);
         printf("A viagem será efetuada a uma velocidade média de %.2fkm/h", media);
-        
-    }
-    return (0);
+ 
+        return (0);
 }
 
