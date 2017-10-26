8455e0fd3a5e271a7d6137c12ee889f131397941
Joao Cerqueira <joaodanielcerqueira@hotmail.com>
2017-10-26 12:42:10 +0100

    Update

--- Ficha2/Ex.15/main.c
+++ Ficha2/Ex.15/main.c
@@ -8,25 +8,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define distancia 130
+#define litros 4
 
 int main(int argc, char** argv) {
 
     int viagem;
-    float media;
+    float media, consumo, combustivel;
     puts ("Em quanto tempo pretende fazer a viagem (Minutos)");
     scanf("%d", &viagem);
     
-    media = (float)distancia / (viagem / 60);
+    media = distancia / (viagem / 60.0);
     
     if (media <= 60){
+        combustivel = 5;             
         printf ("Tem que abastecer ao km 79.\n");
-        printf("A viagem será efetuada a uma velocidade média de %.2fkm/h", media);
-    }else if (media > 60 && media <= 120){
+        printf("A viagem será efetuada a uma velocidade média de %.2fkm/h", media);    
+        }else if (media > 60 && media <= 120){
+        combustivel = 6;
         printf ("Tem que abastecer ao km 70.\n");
-        printf("A viagem será efetuada a uma velocidade média de %.2fkm/h", media);
-    }else{
+        printf("A viagem será efetuada a uma velocidade média de %.2fkm/h", media);    
+        }else{
+        combustivel = 7;
         printf ("Tem que abastecer ao km 60.\n");
         printf("A viagem será efetuada a uma velocidade média de %.2fkm/h", media);
+        
     }
     return (0);
 }
