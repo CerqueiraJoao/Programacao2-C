365c57173529e85a42eacb6e5e81a882c61f92ad
Joao Cerqueira <joaodanielcerqueira@hotmail.com>
2017-10-27 17:13:28 +0100

    Ficha3-Ex.14

--- Ficha3/Ex.14/main.c
+++ Ficha3/Ex.14/main.c
@@ -1,1 +1,16 @@
-ugyfdfxgchj
+/* 
+ * File:   main.c
+ * Author: joaod
+ *
+ * Created on 27 de Outubro de 2017, 16:38
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    
+    puts("Ola");
+    
+    return(0);
+}
