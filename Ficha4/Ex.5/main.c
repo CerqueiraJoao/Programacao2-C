303fade1467ac49e6966a069dd7c00a09b7f946a
Joao Cerqueira <joaodanielcerqueira@hotmail.com>
2017-11-08 11:48:43 +0000

    Ficha4-Ex.5

---
+++ Ficha4/Ex.5/main.c
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.c
+ * Author: joaod
+ *
+ * Created on 8 de Novembro de 2017, 11:37
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void fatorial(int num){
+    int i, valor_final = 1;
+    
+    for(i = 1; i <= num; ++i){
+    valor_final = valor_final * i;
+    }
+    printf("O valor em fatorial é %d", valor_final);
+}
+int main(int argc, char** argv) {
+
+    int num;
+    puts("Indique o numero para determinar o fatorial ");
+    scanf("%d", &num);
+    
+    fatorial(num);
+    
+    return (0);
+}
+
