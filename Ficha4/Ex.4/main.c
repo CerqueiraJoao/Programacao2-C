7ff240e42e3f598ac59aa4172c26aa9e617da795
Joao Cerqueira <joaodanielcerqueira@hotmail.com>
2017-11-08 11:35:53 +0000

    Ficha4-Ex.4

---
+++ Ficha4/Ex.4/main.c
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.c
+ * Author: joaod
+ *
+ * Created on 8 de Novembro de 2017, 10:53
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float nota(int quantidade){
+    int i;
+    double notas, soma;
+    
+    for(i = 0; i < quantidade; ++i){
+        printf("Introduza a nota do %d teste ", i + 1);
+        scanf("%lf", &notas);
+        soma = soma + notas;
+    }
+    return soma;
+}
+
+int main(int argc, char** argv) {
+
+    int quantidade;
+    double media;
+    
+    puts("Quantas notas quer introduzir?");
+    scanf("%d", &quantidade);
+    
+    media = nota(quantidade);
+    
+    printf("A média das notas é %.2lf", media / quantidade);
+    return (0);
+}
+
