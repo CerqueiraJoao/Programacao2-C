efa832f348c9e6897d08ab70a2747af7a811da41
Joao Cerqueira <joaodanielcerqueira@hotmail.com>
2017-11-10 18:06:57 +0000

    Ficha5-Ex6

---
+++ Ficha5/Ex6/main.c
@@ -0,0 +1,65 @@
+/* 
+ * File:   main.c
+ * Author: joaod
+ *
+ * Created on 10 de Novembro de 2017, 17:47
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_MAX 10
+
+void limparBufferEntrada() {
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF);
+}
+
+void par(int numero[]) {
+    int i;
+
+    printf("\nNumeros pares: ");
+    for (i = 0; i < NUM_MAX; ++i) {
+        if (numero[i] % 2 == 0) {
+            printf(" %d ", numero[i]);
+        }
+    }
+}
+
+void impar(int numero[]) {
+    int i;
+
+    printf("\nNumeros impares: ");
+    for (i = 0; i < NUM_MAX; ++i) {
+        if (numero[i] % 2 != 0) {
+            printf(" %d ", numero[i]);
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    int i, numero[NUM_MAX];
+    char op;
+
+    for (i = 0; i < NUM_MAX; ++i) {
+        puts("Indique o numero");
+        scanf("%d", &numero[i]);
+    }
+    
+    do {
+    limparBufferEntrada();
+    puts("\nDeseja ver os pares (P) ou os impares (I)");
+    scanf("%c", &op);
+
+    
+        if (op == 'P' || op == 'p') {
+            par(numero);
+        } else if (op == 'I' || op == 'i') {
+            impar(numero);
+        } else {
+            puts("Opção Incorreta!!");
+        }
+    } while (!(op == 'P' || op == 'p' || op == 'I' || op == 'i'));
+    
+    return (0);
+}
+
