ec083c428c57bd90fa7cec4641269b304081512a
Joao Cerqueira <joaodanielcerqueira@hotmail.com>
2017-11-29 18:02:22 +0000

    Update

--- Ficha7/Ex3/main.c
+++ Ficha7/Ex3/main.c
@@ -23,25 +23,25 @@ int main(int argc, char** argv) {
             } else if (j == 1) {
                 printf("Teste 1 ");
                 scanf("%f", &matriz[i][j]);
-            }else if (j == 2) {
+            } else if (j == 2) {
                 printf("Teste 2 ");
                 scanf("%f", &matriz[i][j]);
             }
         }
     }
-    
+
     puts("Codigo  Teste 1  Teste 2  Media");
     for (i = 0; i < MAX_LINHAS; ++i) {
         matriz[i][3] = (matriz[i][1] + matriz[i][2]) / 2;
     }
-    
+
     for (i = 0; i < MAX_LINHAS; ++i) {
         puts("");
         for (j = 0; j < MAX_COLUNAS; ++j) {
-            printf(" %.0f ", matriz[i][j]);
+            printf("   %.0f    ", matriz[i][j]);
         }
     }
-     
+
     return (0);
 }
 
